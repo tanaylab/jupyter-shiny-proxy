589ea77b3d941da0c8101f046057055e431bfd5a
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2021-07-25 10:37:20 +0300

    added user config

--- jupyter_shiny_proxy/__init__.py
+++ jupyter_shiny_proxy/__init__.py
@@ -6,23 +6,34 @@ from textwrap import dedent
 def setup_shiny():
     '''Manage a Shiny instance.'''
 
+    user_dir = "/net/mraid14/export/tgdata/db/tgdb/tanaywiz/users/{user}".format(user=getpass.getuser())
+    os.makedirs(user_dir)
+    os.makedirs(os.path.join(user_dir, "apps"))
+    os.makedirs(os.path.join(user_dir, "logs"))
+    os.makedirs(os.path.join(user_dir, "bookmarks"))
+
     name = 'shiny'
     def _get_shiny_cmd(port):
         conf = dedent("""
             run_as {user};
+            preserve_logs on;
             server {{
                 listen {port};
                 location / {{
-                    site_dir {site_dir};
-                    log_dir {site_dir}/logs;
-                    bookmark_state_dir {site_dir}/bookmarks;
+                    site_dir {user_dir}/apps;
+                    log_dir {user_dir}/logs;
+                    bookmark_state_dir {user_dir}/bookmarks;
                     directory_index on;
+
+                    # Increased Idle timeout
+                    app_idle_timeout 0;
+
+                    log_file_mode 0774;
                 }}
             }}
         """).format(
-            user=getpass.getuser(),
-            port=str(port),
-            site_dir=os.getcwd()
+            user_dir=user_dir,
+            port=str(port)            
         )
 
         f = tempfile.NamedTemporaryFile(mode='w', delete=False)
